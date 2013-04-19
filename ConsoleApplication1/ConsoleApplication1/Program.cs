af74048f3a293e31206a1ff72a341c85b2287f65
tanvir <tanvir63@hotmail.com>
2013-04-19 14:15:20 -0400

    only in local copy

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello from team member");
+            Console.Write("Hello from team memberdasfsaf");
         }
     }
 }

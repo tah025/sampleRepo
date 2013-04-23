045f405884101081e117df6ab534422d36d6f932
tanvir <tanvir63@hotmail.com>
2013-04-23 16:08:16 -0400

    1

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Consolve.Write("Hello from upstream");
+            Console.Write("Hello from upstream");
         }
     }
 }
